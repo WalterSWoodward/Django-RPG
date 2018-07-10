96280b14d142788b7e108ea6b59dd90fa40babd6
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-07-10 16:47:34 -0400

    testdata imported into django

--- armory/models.py
+++ armory/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Item(models.Model):
+    # TODO: Investigate that item_id might actually be unnecessary from docs
     item_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     value = models.IntegerField(default=0)
